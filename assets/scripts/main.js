517629b6295ff07de0cacda6ad8ec3bb18668600
balexdalex42 <atatoian@ucsd.edu>
2025-05-29 21:02:08 -0700

    Added slight relative path so that paths cold be read into lab8 for website

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -57,7 +57,7 @@ function initializeServiceWorker() {
   if("serviceWorker" in navigator){
     window.addEventListener('load', async function(){
       try{
-        const registration = await navigator.serviceWorker.register('/sw.js', {
+        const registration = await navigator.serviceWorker.register('./sw.js', {
           scope: "/",
         });
         if(!registration.installing){
